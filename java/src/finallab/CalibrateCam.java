45b4de46409fe7897ae83232ba121eece495e96c
keemax <keemax@umich.edu>
2013-03-28 18:40:19 -0400

    initial camera calibration

--- java/src/finallab/CalibrateCam.java
+++ java/src/finallab/CalibrateCam.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseListener;
 import java.io.*;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import org.openkinect.freenect.Context;
 import org.openkinect.freenect.Device;
@@ -23,8 +24,23 @@ public class CalibrateCam
 	JFrame window;
 	KinectRGBVideo rgbVideo;
 	
+	final static int C_X = KinectRGBVideo.WIDTH / 2;
+	final static int C_Y = KinectRGBVideo.HEIGHT / 2;
+	
+	ArrayList<Integer> arr_p_x;
+	ArrayList<Integer> arr_p_y;
+	ArrayList<Double[]> arr_r_x;
+	ArrayList<Double[]> arr_r_y;
+	
+	Matrix r;
+	Matrix p;
 	CalibrateCam()
 	{
+		arr_p_x = new ArrayList<Integer>();
+		arr_p_y = new ArrayList<Integer>();
+		arr_r_x = new ArrayList<Double[]>();
+		arr_r_y = new ArrayList<Double[]>();
+		
 		ctx = Freenect.createContext();
 		if (ctx.numDevices() > 0) {
 			kinect = ctx.openDevice(0);
@@ -34,21 +50,42 @@ public class CalibrateCam
 		window = new JFrame("calibrate camera");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		rgbVideo = new KinectRGBVideo(kinect);
+		rgbVideo.addMouseListener(ml);
 		window.setContentPane(rgbVideo);
 	}
 	
-	MouseListener calibrateTheta = new MouseAdapter() {
+	MouseListener ml = new MouseAdapter() {
 		public void mouseClicked(MouseEvent e) {
 			Point p = e.getPoint();
-			System.out.println("x: " + p.x + ", y: " + p.y);
-			System.out.println("x dist: " + (p.x - KinectRGBVideo.WIDTH) + ", y dist: " + (p.y - KinectRGBVideo.HEIGHT));
+			//pixel points of click
+			int pX = p.x - C_X;
+			int pY = C_Y - p.y;
+			
+			//real world points of click
+			double rZ = .5;
+			//double depth = getDepth(x, y);
+			double rX = Double.parseDouble(JOptionPane.showInputDialog("enter real world x value"));
+			double rY = Double.parseDouble(JOptionPane.showInputDialog("enter real world y value"));
+			System.out.println("x: " + pX + ", y: " + pY);
+			
+			arr_p_x.add(pX);
+			arr_p_y.add(pY);
+			arr_r_x.add(new Double[] {rX, rY, rZ, 1d, 0d, 0d, 0d, 0d, -pX*rX, -pX*rY, -pX*rZ});
+			arr_r_y.add(new Double[] {0d, 0d, 0d, 0d, rX, rY, rZ, 1d, -pY*rX, -pY*rY, -pY*rZ});
+			
 		}
 	};
+	
+	private void createMatrix() {
+//		ArrayList<Integer> r_array = 
+	}
+	
 	public static void main(String [] args) {
 		CalibrateCam cc = new CalibrateCam();
 	}
 	
 	
+	//q = (r^tr)^-1r^tp
 	
 
 
