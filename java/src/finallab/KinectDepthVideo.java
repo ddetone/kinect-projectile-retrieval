676cb4199d90dccd4c82b84b1071a74d1a673e95
keemax <keemax@umich.edu>
2013-04-17 06:16:29 -0400

    small changes

--- java/src/finallab/KinectDepthVideo.java
+++ java/src/finallab/KinectDepthVideo.java
@@ -22,11 +22,6 @@ public class KinectDepthVideo extends KinectVideo {
 	public static int MAX_FRAMES = 100;
 	public static int THRESH = 50;
 
-	//figured the noisy pixels switch back and forth a lot so if we 
-	//stop allowing pixels to be valid after they've switched a certain 
-	//number of times, it might help eliminate them.
-	public static int SWITCHES = 200;
-
 	private double [] depthAvgs;
 	private short [] validPixels;
 	private int [] switchCount;
@@ -100,7 +95,7 @@ public class KinectDepthVideo extends KinectVideo {
 							if (depthAvgs[i] == 2047) {
 								depthAvgs[i] = depth;
 							}
-							if (/*depth<1000 &&*/ (depthAvgs[i] - (double)depth) > THRESH && switchCount[i] < SWITCHES) {
+							if (/*depth<1000 &&*/ (depthAvgs[i] - (double)depth) > THRESH) {
 								valid = true;
 								validPixels[i] = (short)depth;		
 								switchCount[i]++;	
@@ -108,14 +103,14 @@ public class KinectDepthVideo extends KinectVideo {
 							//double depthFactor = (((depthAvgs[i] * (1.0-pg.gd("learning")) * (double)numFrames) + (pg.gd("learning")*(double)depth)) / (double)(numFrames + 1));
 							depthAvgs[i] = (((depthAvgs[i] * (double)numFrames) + (double)depth) / (double)(numFrames + 1));
 						}
-						if (botLoc != null) {
-							if (i == WIDTH*botLoc.y + botLoc.x) {
-								Point estimatedPicPoint = new Point(botLoc.x-C_X,C_Y-botLoc.y);
-								Point3D estimate = getWorldCoords(estimatedPicPoint,raw_depth_to_meters(depth));
-								System.out.println("x: "+ estimate.x +" y: "+ estimate.y +" z: " + estimate.z);
-								System.out.println("botLoc depth: " + depth + ", avg: " + depthAvgs[i]);
-							}
-						}
+//						if (botLoc != null) {
+//							if (i == WIDTH*botLoc.y + botLoc.x) {
+//								Point estimatedPicPoint = new Point(botLoc.x-C_X,C_Y-botLoc.y);
+//								Point3D estimate = getWorldCoords(estimatedPicPoint,raw_depth_to_meters(depth));
+//								System.out.println("x: "+ estimate.x +" y: "+ estimate.y +" z: " + estimate.z);
+//								System.out.println("botLoc depth: " + depth + ", avg: " + depthAvgs[i]);
+//							}
+//						}
 						else {
 							depthAvgs[i] = (((depthAvgs[i] * (double)numFrames) + (double)depthAvgs[i]) / (double)(numFrames + 1));
 						}
